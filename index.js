459d105cb1253ff9826a5819218c18585f7bc711
kshitijk83 <kshitijk83@gmail.com>
2020-03-24 16:57:25 +0530

    asd

---
+++ index.js
@@ -0,0 +1,33 @@
+const { GraphQLServer } = require('graphql-yoga');
+
+let links = [{
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL'
+  }]
+
+const typeDefs = `
+    type Query{
+        info: String!
+        feed: [Link!]!
+    }
+
+    type Link{
+        id: ID!
+        description: String!
+        url: String!
+    }
+`
+
+const resolvers = {
+    Query:{
+        info:()=>'This is the API of a Hackernews Clone',
+        feed: ()=>links
+    }
+}
+
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers
+});
+server.start(()=>console.log("Server is running on http://localhost:4000"))
