8bc5ac848d599725e862695e2dba009184188a3f
kshitijk83 <kshitijk83@gmail.com>
2020-03-25 12:09:04 +0530

    mutation

--- index.js
+++ index.js
@@ -5,29 +5,42 @@ let links = [{
     url: 'www.howtographql.com',
     description: 'Fullstack tutorial for GraphQL'
   }]
-
-const typeDefs = `
-    type Query{
-        info: String!
-        feed: [Link!]!
-    }
-
-    type Link{
-        id: ID!
-        description: String!
-        url: String!
-    }
-`
-
+let idCount = links.length;
 const resolvers = {
     Query:{
         info:()=>'This is the API of a Hackernews Clone',
-        feed: ()=>links
+        feed: ()=>links,
+        link: (parent, args)=>{
+            link = links.find(el=>el.id===args.id);
+            return link;
+        }
+    },
+    Mutation:{
+        post: (parent, args)=>{
+            const link={
+                id: `link-${idCount++}`,
+                description: args.description,
+                url: args.url,
+            }
+            links.push(link);
+            return link;
+        },
+        updateLink: (parent, args)=>{
+            const link = links.find(el=>el.id===args.id);
+            link.url = args.url;
+            link.description=args.description;
+            return link;
+        },
+        deleteLink:(parent, args)=>{
+            const index = links.findIndex(el=>el.id===args.id);
+            links.splice(index, 1);
+            return links;
+        }
     }
 }
 
 const server = new GraphQLServer({
-    typeDefs,
+    typeDefs: './src/schema.graphql',
     resolvers
 });
 server.start(()=>console.log("Server is running on http://localhost:4000"))
