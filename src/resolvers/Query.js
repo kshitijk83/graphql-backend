e3cf30e758a6133b2d6a6fae4a9547c2d11e5c7d
kshitijk83 <kshitijk83@gmail.com>
2020-04-01 11:54:09 +0530

    pagination sorting

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,5 +1,26 @@
-function feed(parent, args, context, info){
-    return context.prisma.links();
+async function feed(parent, args, context, info){
+    const where = args.filter?{
+        OR:[
+            { description_contains: args.filter },
+            { url_contains: args.filter }
+        ]
+    }:{};
+    const links = await context.prisma.links({
+        where,
+        skip: args.skip,
+        first: args.first,
+        orderBy: args.orderBy
+    });
+
+    const count = await context.prisma.linksConnection({
+        where,
+    })
+    .aggregate()
+    .count()
+    return {
+        links,
+        count
+    };
 }
 
 module.exports={
